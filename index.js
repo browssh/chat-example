b14b0fe4a187e7b5c21f6bcd27737e756cd514bb
Basant Besra <besrabasant@gmail.com>
2018-09-16 21:26:20 +0530

    -  Add Functionality: Connect SSH to FrontendClient using socket.io - Add .env

--- index.js
+++ index.js
@@ -1,56 +1,71 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http)
-var Client = require('ssh2').Client;
+require("dotenv").config();
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var Client = require("ssh2").Client;
 
 var conn = new Client();
-conn.connect({
-    host: 'localhost',
-    port: 22,
-    username: 'radha',
-    password:'radha'
+
+const clientCreds = {
+  host: process.env.SSH_HOST,
+  port: process.env.SSH_PORT,
+  username: process.env.SSH_USER,
+  password: process.env.SSH_PASS
+};
+
+var outputmsg = "";
+
+var sshshell = null;
+
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("clientcmd", msg => {
+    // outputmsg = "";
+    // console.log("message: " + msg);
+    sshshell.write(`${msg}\n`);
+  });
 });
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-var outputmsg = '';
-var func = (msg) =>{
 
+conn
+  .on("ready", () => {
+    console.log("Client :: ready");
 
-}
-
-conn.on('ready', () => {
-    console.log('Client :: ready');
     conn.shell(function(err, stream) {
-        if (err) throw err;
-        stream.on('close', function() {
-            console.log('Stream :: close');
-            conn.end();
-        }).on('data', function(data) {
-            console.log('STDOUT: ' + data);
-            outputmsg += data;
-        }).stderr.on('data', function(data) {
-            console.log('STDERR: ' + data);
-            outputmsg+=data;
+      if (err) throw err;
+
+      sshshell = stream;
+
+      sshshell
+        .on("close", function() {
+          console.log("Stream :: close");
+          conn.end();
+        })
+        .on("data", function(data) {
+          //   outputmsg += data;
+          //   console.log("STDOUT: " + outputmsg);
+          io.emit("sshresponse", `${data}`);
+        })
+        .stderr.on("data", function(data) {
+          //   outputmsg += data;
+          // console.log("STDERR: " + outputmsg);
+          io.emit("sshresponse", `${data}`);
         });
 
-        stream.end("ls\n");
+      sshshell.write("ls\n");
     });
-});
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-       console.log('user disconnected');
-    });
-    socket.on('chat message', (msg) => {
-       outputmsg=''
-       console.log('message: '+ msg);
-       /*func(msg);*/
-        io.emit('chat message', msg); /* for chat client, just show msg */
-      /*  io.emit('chat message', outputmsg);*/ /* for shell*/
-    });
+  })
+  .connect(clientCreds);
+
+/**
+ * HTTP SERVER.
+ */
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 http.listen(3000, () => {
-   console.log('listening on *:3000') ;
+  console.log("listening on *:3000");
 });
